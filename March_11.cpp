74ae556a28af481a3a14b2abf6f4d8de611201cc
Kulvardhan Singh Rathore <kulsrstudy6802@gmail.com>
2024-03-11 13:40:34 +0530

    March 11 Done

---
+++ March_11.cpp
@@ -0,0 +1,60 @@
+// 791. Custom Sort String
+/*
+You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
+Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
+Return any permutation of s that satisfies this property.
+
+Example 1:
+Input:  order = "cba", s = "abcd"
+Output:  "cbad"
+Explanation: "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a".
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+
+Example 2:
+Input:  order = "bcafg", s = "abcd"
+Output:  "bcad"
+Explanation: The characters "b", "c", and "a" from order dictate the order for the characters in s. The character "d" in s does not appear in order, so its position is flexible.
+Following the order of appearance in order, "b", "c", and "a" from s should be arranged as "b", "c", "a". "d" can be placed at any position since it's not in order. The output "bcad" correctly follows this rule. Other arrangements like "bacd" or "bcda" would also be valid, as long as "b", "c", "a" maintain their order.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+string CustomSortString(string order, string s)
+{
+    unordered_map<char, int> f;
+    for (char ch : s)
+        f[ch]++;
+
+    string res = "";
+
+    for (char od : order)
+    {
+        if (f.find(od) != f.end())
+        {
+            while (f[od])
+            {
+                res += od;
+                f[od]--;
+            }
+        }
+    }
+
+    for (char i : s)
+    {
+        if (f[i] >= 1)
+            res += i;
+    }
+
+    return res;
+}
+int main()
+{
+    string order;
+    string s;
+    cin >> order;
+    cin >> s;
+
+    cout << CustomSortString(order, s);
+
+    return 0;
+}
